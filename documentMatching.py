39fd2d706d0e920962570e23af000b1d602513aa
danielharada <haradad76@gmail.com>
2014-12-12 14:59:14 -0800

    Version 2

--- documentMatching.py
+++ documentMatching.py
@@ -5,9 +5,9 @@ these lists.  These files are then placed into groups based on
 similarity in contents (word_set).  The goal is: given a document,
 find other documents that have similar content.
 """
-import FileInteraction
+import fileInteraction
 
-grouping_ratio = 0.25  # Need to match on 25% of the words in a document list to count as related
+min_similarity = 10  # Minimum similarity score needed to count as match
 
 
 def removeCommonWords(document_word_set):
@@ -41,44 +41,57 @@ def removeCommonWords(document_word_set):
     return document_word_set - common_words
 
 def getTextAndCleanIt(directory):
-    document_dict = FileInteraction.readAllFilesToSets(directory)  # Will need to change this line based on data source
+    document_dict = fileInteraction.readAllFilesToSets(directory)  # Will need to change this line based on data source
     for document in document_dict:
         word_set = document_dict[document]
         document_dict[document] = removeCommonWords(word_set)
     return document_dict
 
-def createGroups(document_dict):
-    groups = {}
-    i = 1
+def generateWordCount(document_dict):
+    word_count = {}
     for document in document_dict:
-        groups['group{}'.format(i)] = [key for key in document_dict]
-        removeUnrelatedDocumentsFromGroup(groups['group{}'.format(i)], document_dict, document)
-        i += 1
+        for word in document_dict[document]:
+            word_count[word] = word_count[word] + 1 if word in word_count else 1
+    return word_count
+
+def createGroups(document_dict, word_count):
+    groups = {}
+    for document in document_dict:
+        doc_name = document[60:]  #  Slice depends on file path
+        groups[doc_name] = []
+        addDocumentsToGroup(groups, document, document_dict, word_count)
     printGroups(groups)
 
-def removeUnrelatedDocumentsFromGroup(group, document_dict, document_to_compare_to):
-    delete_list = []
-    for document in group:
+def addDocumentsToGroup(groups, document_to_compare_to, document_dict, word_count):
+    compare_to_word_set = document_dict[document_to_compare_to]
+    compare_to_name = document_to_compare_to[60:]  #  Slice depends on file path
+    all_other_docs = [doc for doc in document_dict]
+    all_other_docs.remove(document_to_compare_to)
+    #print('all_other_docs is:')
+    #print(all_other_docs)
+    for document in all_other_docs:
         comparee_word_set = document_dict[document]
-        compare_to_word_set = document_dict[document_to_compare_to]
-        sub_set = comparee_word_set & compare_to_word_set
-        if len(sub_set) < grouping_ratio*len(compare_to_word_set):
-            delete_list.append(document)
-
-    removeElements(group, delete_list)
-
-def removeElements(remove_from_list, remove_these_items):
-    for item in remove_these_items:
-        remove_from_list.remove(item)
-
+        comparee_name = document[60:]  #  Slice depends on file path.
+        #print('We are comparing {} to {}'.format(comparee_name, compare_to_name))
+        #print('The {} group currently consists of: '.format(compare_to_name))
+        #print(groups[compare_to_name])
+        if comparee_name not in groups[compare_to_name]:
+            sub_set = comparee_word_set & compare_to_word_set
+            similarity = 0
+            for word in sub_set:
+                similarity += 1/word_count[word]
+            print(similarity)  #for checking, remove this line
+            if similarity >=  min_similarity:
+                groups[compare_to_name].append(comparee_name)
 
 def printGroups(groups):
     for group in groups:
-        print('{} is:'.format(group))
+        print('The {} group is:'.format(group))
         print(groups[group])
         print('')
 
 if __name__ == '__main__':
     directory = "C:\\Users\\student\\Documents\\Python\\Document Grouping\\Resumes"
     document_dict = getTextAndCleanIt(directory)
-    groups = createGroups(document_dict)
+    word_count = generateWordCount(document_dict)
+    groups = createGroups(document_dict, word_count)
